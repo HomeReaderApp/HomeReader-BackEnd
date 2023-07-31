13126e167839e21327ad7be2630da7b4382ed580
Coder-Nicki <nicki5389@gmail.com>
2023-07-31 20:25:53 +1000

    updated validate function

--- src/middlewares/auth_middlewares.js
+++ src/middlewares/auth_middlewares.js
@@ -1,27 +1,4 @@
 
-// const { verifyToken } = require('../services/auth_services')
-
-// const validateRequest = (request, response, next) => {
-//     try {
-//         if(request.headers.authorization){
-//             const token = request.headers.authorization.split(" ")[1]
-//             if (!token){
-//                 throw new Error ("A token is required for authentication")
-//             }
-//             const decoded = verifyToken(token)
-//             request.user = decoded
-//             return next()
-//         } else {
-//             throw new Error ("Not authenticated for this action")
-//         }
-//         return next()
-//     } catch (error) {
-//         next(error)
-//     }
-// }
-
-// module.exports = validateRequest
-
 const { verifyToken } = require('../services/auth_services');
 
 const validateRequest = (request, response, next) => {
@@ -33,7 +10,8 @@ const validateRequest = (request, response, next) => {
       }
       const decoded = verifyToken(token);
       request.user = decoded;
-      next(); // Move this line inside the if block to proceed to the next middleware/route handler only if authenticated.
+      next(); 
+
     } else {
       throw new Error('Not authenticated for this action');
     }
